d5f9483086118007cf6395860bb11ebc7691fbdb
RahmatussawalH <116995190+RahmatussawalH@users.noreply.github.com>
2022-10-30 13:19:32 +0700

    Add files via upload
    RAHMATUSSAWAL HIDAYAT - 1101221040

---
+++ kategori umur.py
@@ -0,0 +1,16 @@
+﻿print("KATEGORI UMUR")
+print("Silahkan Masukan Tahun Lahir Anda")
+lahir = int(input())
+usia = 2022 - lahir
+print("Usia Anda " + str(usia) + " Tahun")
+print("Anda Termasuk Golongan")
+if usia <= 11:
+    print("Anak-Anak")
+else:
+    if usia >= 12 and usia <= 25:
+        print("Remaja")
+    else:
+        if usia >= 26 and usia <= 45:
+            print("Dewasa")
+        else:
+            print("Lansia")
